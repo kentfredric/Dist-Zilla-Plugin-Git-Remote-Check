175a03ce6fb38fa0995819e975a90514a6637f35
Kent Fredric <kentfredric@gmail.com>
2011-10-13 11:37:53 +1300

    Make messages a little tidier, and get git commands in the right order

--- lib/Dist/Zilla/Role/Git/Remote/Check.pm
+++ lib/Dist/Zilla/Role/Git/Remote/Check.pm
@@ -15,10 +15,31 @@ requires 'branch';
 
 has 'report_commits' => ( isa => 'Int', is => 'rw', default => 5 );
 
+sub _incomming_commits {
+  my $self = shift;
+  return ( $self->git->rev_list( '--oneline', $self->remote_branch, '--not', $self->branch, ) );
+}
+
+# this checks the inverse, ie: that local is *not* ahead of remote, only used for
+# testing purposes.
+sub _outgoing_commits {
+  my $self = shift;
+
+  # $self->log('[TESTING] Doing inverse check, making sure we\'re ahead');
+  # $self->log([ '[TESTING] remote: %s  local: %s', $self->remote_branch, $self->branch ]);
+  my (@commits) = $self->git->rev_list( '--oneline', $self->branch, '--not', $self->remote_branch );
+
+  #  require Data::Dump;
+  #  $self->log(['[TESTING] %s', Data::Dump::dump( \@commits ) ]);
+  return @commits;
+}
+
 sub check_remote {
   my $self = shift;
 
-  my (@commits) = $self->git->rev_list( '--not',$self->branch, $self->remote_branch );
+  my (@commits) = $self->_incomming_commits;
+
+  #my (@commits) = $self->_outgoing_commits;
 
   return unless @commits;
 
@@ -27,10 +48,10 @@ sub check_remote {
   my $commits_displayed = scalar @selected_commits;
 
   my $message = <<'EOF';
-  %d commits visible upstream on '%s' not visible on '%s'.
-  Either merge with '%s', rebase on '%s', or anihilate upstream with 'git push -f '
-  %d most recent commits:
-    %s
+There are %d commits visible upstream on '%s' not visible on '%s'.
+Either merge with '%s', rebase on '%s', or anihilate upstream with 'git push -f '
+The %d most recent commits are:
+ %s
 EOF
 
   require Data::Dump;

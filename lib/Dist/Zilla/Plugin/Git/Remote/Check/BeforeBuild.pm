d58ad9d1c7eaa8c4698cc52bcd518920bd469c09
Kent Fredric <kentfredric@gmail.com>
2011-10-15 02:18:16 +1300

    Document Dist::Zilla::Plugin::Git::Remote::Check::BeforeBuild
    Rather Exhaustively in fact.
    
    Weaver.ini tweaked to have ROLES and PARAMS sections.
    
    Perlcritic.rc added to support 'boolean' because apparently that's not a
    word.

--- lib/Dist/Zilla/Plugin/Git/Remote/Check/BeforeBuild.pm
+++ lib/Dist/Zilla/Plugin/Git/Remote/Check/BeforeBuild.pm
@@ -42,17 +42,32 @@ use Moose;
 
 =cut
 
+=role C<Dist::Zilla::Role::BeforeBuild>
+
+Causes this plugin to be executed during L<Dist::Zilla>'s "Before Build" phase.
+( L</before_build> )
+
+=cut
+
 with 'Dist::Zilla::Role::BeforeBuild';
-with 'Dist::Zilla::Role::Git::LocalRepository';
-with 'Dist::Zilla::Role::Git::Remote';
-with 'Dist::Zilla::Role::Git::Remote::Branch';
-with 'Dist::Zilla::Role::Git::Remote::Update';
 
-has 'branch' => ( isa => 'Str', is => 'rw', default => 'master' );
+=method C<before_build>
 
-with 'Dist::Zilla::Role::Git::Remote::Check';
+Executes code before building the release.
 
-has '+_remote_branch' => ( lazy => 1, default => sub { shift->branch } );
+=over 4
+
+=item 1. 
+
+Updates the L</remote> via L<Dist::Zilla::Role::Git::Remote::Update/remote_update>
+
+=item 2.
+
+Checks the L</remote> via L<Dist::Zilla::Role::Git::Remote::Check/check_remote>
+
+=back
+
+=cut
 
 sub before_build {
   my $self = shift;
@@ -60,6 +75,123 @@ sub before_build {
   $self->check_remote;
   return 1;
 }
+
+=role C<Dist::Zilla::Role::Git::LocalRepository>
+
+Provides a L</git> method that returns a C<Git::Wrapper> instance for the
+current C<Dist::Zilla> project.
+
+
+=method C<git>
+
+Returns a L<Git::Wrapper> instance for the current L<Dist::Zilla> projects
+C<git> Repository.
+
+=cut
+
+with 'Dist::Zilla::Role::Git::LocalRepository';
+
+=role C<Dist::Zilla::Role::Git::Remote>
+
+Provides a L</remote> method which always returns a validated C<remote> name,
+optionally accepting it being specified manually to something other than
+C<origin> via the parameter L</remote_name>
+
+=method C<remote>
+
+Returns a validated remote name. Configured via L</remote_name> parameter.
+
+=cut
+
+=param C<remote_name>
+
+The name of the repository to use as specified in C<.git/config>.
+
+Defaults to C<origin>, which is usually what you want.
+
+=cut
+
+with 'Dist::Zilla::Role::Git::Remote';
+
+=role C<Dist::Zilla::Role::Git::Remote::Branch>
+
+Provides a L</remote_branch> method which combines the value returned by
+L</remote> with a user specified branch name and returns a fully qualified
+remote branch name.
+
+=method C<remote_branch>
+
+Returns a fully qualified branch name for the parameter specified as
+C<remote_branch> by combining it with L</remote>, and defaulting to the value of
+L</branch> if not assigned explicitly.
+
+=param C<remote_branch>
+
+The branch name on the remote.
+
+e.g.: For C<origin/master> use C<master> with C<remote_name = origin>
+
+Defaults to the same value as L</branch>
+
+=cut
+
+with 'Dist::Zilla::Role::Git::Remote::Branch';
+
+=role C<Dist::Zilla::Role::Git::Remote::Update>
+
+Provides a L</remote_update> method which updates a L</remote> in L</git> 
+
+=method C<remote_update>
+
+Performs C<git remote update $remote_name> on L</git> for the remote L</remote>
+
+=param C<do_update>
+
+A boolean value that specifies whether or not to execute the update.
+
+Default value is C<1> / true.
+
+=cut
+
+with 'Dist::Zilla::Role::Git::Remote::Update';
+
+=method C<branch>
+
+The local branch to check against the remote one. Defaults to 'master'
+
+=param C<branch>
+
+The local branch to check against the remote one. Defaults to 'master'
+
+=cut
+
+has 'branch' => ( isa => 'Str', is => 'rw', default => 'master' );
+
+=role C<Dist::Zilla::Role::Git::Remote::Check>
+
+Provides L</check_remote> which compares L</branch> and L</remote_branch> and
+asserts L</remote_branch> is not ahead of L</branch>
+
+=method C<check_remote>
+
+Compare L</branch> and L</remote_branch> making sure that L</branch> is the more
+recent of the 2.
+
+=param C<report_commits>
+
+In the event the remote is ahead, this C<Int> dictates the maximum number of
+commits to print to output. 
+
+Defaults to C<5>
+
+=cut
+
+
+with 'Dist::Zilla::Role::Git::Remote::Check';
+
+has '+_remote_branch' => ( lazy => 1, default => sub { shift->branch } );
+
+
 
 __PACKAGE__->meta->make_immutable;
 no Moose;

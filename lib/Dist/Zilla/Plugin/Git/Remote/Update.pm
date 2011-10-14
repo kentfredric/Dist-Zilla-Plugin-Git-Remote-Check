656aec782a7b1d6f02bb23906fde4629a7a0f48f
Kent Fredric <kentfredric@gmail.com>
2011-10-15 02:51:04 +1300

    Document Git::Remote::Update

--- lib/Dist/Zilla/Plugin/Git/Remote/Update.pm
+++ lib/Dist/Zilla/Plugin/Git/Remote/Update.pm
@@ -9,11 +9,6 @@ package Dist::Zilla::Plugin::Git::Remote::Update;
 
 use Moose;
 
-with 'Dist::Zilla::Role::BeforeRelease';
-with 'Dist::Zilla::Role::Git::LocalRepository';
-with 'Dist::Zilla::Role::Git::Remote';
-with 'Dist::Zilla::Role::Git::Remote::Update';
-
 =head1 SYNOPSIS
 
 This Module is mostly intended to be used in conjunction with other things,
@@ -38,11 +33,85 @@ before you release, and remotes don't usually have any impact on things in the r
 
 =cut
 
+=role C<Dist::Zilla::Role::BeforeRelease>
+
+Causes this plugin to be executed during L<Dist::Zilla>'s "Before Release" phase.
+( L</before_release> )
+
+=cut
+
+with 'Dist::Zilla::Role::BeforeRelease';
+
+=method C<before_release>
+
+Updates the L</remote> via L<Dist::Zilla::Role::Git::Remote::Update/remote_update>, before releasing.
+
+=back
+
+=cut
+
 sub before_release {
   my $self = shift;
   $self->remote_update;
   return 1;
 }
+
+=role C<Dist::Zilla::Role::Git::LocalRepository>
+
+Provides a L</git> method that returns a C<Git::Wrapper> instance for the
+current C<Dist::Zilla> project.
+
+
+=method C<git>
+
+Returns a L<Git::Wrapper> instance for the current L<Dist::Zilla> projects
+C<git> Repository.
+
+=cut
+
+
+with 'Dist::Zilla::Role::Git::LocalRepository';
+
+=role C<Dist::Zilla::Role::Git::Remote>
+
+Provides a L</remote> method which always returns a validated C<remote> name,
+optionally accepting it being specified manually to something other than
+C<origin> via the parameter L</remote_name>
+
+=method C<remote>
+
+Returns a validated remote name. Configured via L</remote_name> parameter.
+
+=cut
+
+=param C<remote_name>
+
+The name of the repository to use as specified in C<.git/config>.
+
+Defaults to C<origin>, which is usually what you want.
+
+=cut
+
+
+with 'Dist::Zilla::Role::Git::Remote';
+
+=role C<Dist::Zilla::Role::Git::Remote::Update>
+
+Provides a L</remote_update> method which updates a L</remote> in L</git>
+
+=method C<remote_update>
+
+Performs C<git remote update $remote_name> on L</git> for the remote L</remote>
+
+=param C<do_update>
+
+A boolean value that specifies whether or not to execute the update.
+
+Default value is C<1> / true.
+
+=cut
+
+with 'Dist::Zilla::Role::Git::Remote::Update';
 
 no Moose;
 __PACKAGE__->meta->make_immutable;

627863eac011a76476a947ba76096ac434dc3a23
Kent Fredric <kentfredric@gmail.com>
2013-11-23 18:48:19 +1300

    Pod Coverage

--- lib/Dist/Zilla/Role/Git/RemoteNames.pm
+++ lib/Dist/Zilla/Role/Git/RemoteNames.pm
@@ -45,10 +45,30 @@ has 'remote_names' => (
   init_arg   => undef,
 );
 
+=method C<has_remote_name>
+
+    if ( $self->has_remote_name( $name ) ) {
+
+    }
+
+Returns true if C<git> reports C<$name> is a remote name that exists.
+
+=cut
+
 sub has_remote_name {
   my ( $self, $remote_name ) = @_;
   return $self->_has_remote_name( sub { $_ eq $remote_name } );
 }
+
+=method C<get_valid_remote_name>
+
+    my $remote = $self->get_valid_remote_name( $remotename );
+
+Returns C<$remotename> as long as C<$remotename> is a valid remote according to C<git>
+
+Raises a fatal error otherwise.
+
+=cut
 
 sub get_valid_remote_name {
   my ( $self, $remote_name ) = @_;
